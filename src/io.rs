28e0bb3a157adcef012eeb3f35f61cda2cba7862
Bryan E. Barnhart <thebryanb@hotmail.com>
2016-08-06 19:14:15 -0600

    created and implemented input trait

--- src/io.rs
+++ src/io.rs
@@ -11,3 +11,8 @@ pub trait Display {
     fn set(&self, row:usize, col:usize, state:Pixel) -> Result<(),()>;
     fn refresh(&self);
 }
+
+pub trait Input {
+    fn get_keys(&self) -> Vec<u8>;
+    fn get_key(&self) -> u8;
+}
